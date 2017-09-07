3fd84a2b72e86b7a4e0dc703c357171967f696c8
Ryan Kienstra <ryan.kienstra@xwp.co>
2017-09-07 00:25:43 -0500

    BSG-1 : Begin PHPUnit tests for class Plugin.
    Test init(), load_files(), and init_classes().
    @todo: finish the tests for this class.

---
+++ tests/php/test-class-plugin.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Tests for class Plugin.
+ *
+ * @package BootstrapSwipeGallery
+ */
+
+namespace BootstrapSwipeGallery;
+
+/**
+ * Tests for class Plugin.
+ *
+ * @package BootstrapSwipeGallery
+ */
+class Test_Class_Plugin extends \WP_UnitTestCase {
+
+	/**
+	 * Instance of plugin.
+	 *
+	 * @var object
+	 */
+	public $plugin;
+
+	/**
+	 * Setup.
+	 *
+	 * @inheritdoc
+	 */
+	public function setUp() {
+		parent::setUp();
+		$this->plugin = Plugin::get_instance();
+	}
+
+	/**
+	 * Test construct().
+	 *
+	 * @see Plugin::__construct().
+	 */
+	public function test_get_instance() {
+		$this->assertEquals( Plugin::get_instance(), $this->plugin );
+		$this->assertEquals( __NAMESPACE__ . '\Plugin', get_class( Plugin::get_instance() ) );
+		$this->assertEquals( '1.0.4', $this->plugin->version );
+		$this->assertEquals( 'bootstrap-swipe-gallery', $this->plugin->slug );
+	}
+
+	/**
+	 * Test init().p
+	 *
+	 * @see Plugin::init().
+	 */
+	public function test_init() {
+		$this->plugin->init();
+		$this->assertTrue( class_exists( __NAMESPACE__ . '\Modal_Carousel' ) );
+
+		$this->assertEquals( __NAMESPACE__ . '\Modal_Setup', get_class( $this->plugin->components['modal_setup'] ) );
+		$this->assertEquals( __NAMESPACE__ . '\Options', get_class( $this->plugin->components['options'] ) );
+
+		$this->assertEquals( 10, has_action( 'init', array( $this->plugin, 'textdomain' ) ) );
+		$this->assertEquals( 10, has_action( 'wp_enqueue_scripts', array( $this->plugin, 'enqueue_assets' ) ) );
+		$this->assertEquals( 10, has_action( 'wp_enqueue_scripts', array( $this->plugin, 'localize_asset' ) ) );
+	}
+
+	/**
+	 * Test load_files().
+	 *
+	 * @see Plugin::load_files().
+	 */
+	public function test_load_files() {
+		$this->assertTrue( class_exists( __NAMESPACE__ . '\Modal_Carousel' ) );
+		$this->assertTrue( class_exists( __NAMESPACE__ . '\Modal_Setup' ) );
+		$this->assertTrue( class_exists( __NAMESPACE__ . '\Options' ) );
+	}
+
+	/**
+	 * Test init_classes().
+	 *
+	 * @see Plugin::init_classes().
+	 */
+	public function test_init_classes() {
+		$this->plugin->init_classes();
+		$this->assertEquals( __NAMESPACE__ . '\Modal_Setup', get_class( $this->plugin->components['modal_setup'] ) );
+		$this->assertEquals( __NAMESPACE__ . '\Options', get_class( $this->plugin->components['options'] ) );
+		$this->assertEquals( 10, has_action( 'loop_end', array( $this->plugin->components['modal_setup'], 'create_galleries' ) ) );
+		$this->assertEquals( 10, has_action( 'admin_menu', array( $this->plugin->components['options'], 'plugin_page' ) ) );
+	}
+
+}
